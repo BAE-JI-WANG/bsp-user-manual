4c3c38a9b63f1e7a0b32670a2532b339859dc48a
Jay Shin <mycoolade@gmail.com>
2017-12-25 23:57:34 +0900

    1차 매뉴얼 정리

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@
  *
  */
 var	gulp = require('gulp'),
+	clean = require('gulp-clean'),                      // 개발 혹은 빌드 전 이전 파일들을 꺠끗하게 비우고 시작함
+    stripComment = require('gulp-strip-comments'),      // 빌드할때 써먹을 설정입니다.  
     watch = require('gulp-watch'),
     inquirer = require('inquirer'),
     browserSync = require('browser-sync').create(),    // 브라우저 동기화...
@@ -14,14 +16,14 @@ var	gulp = require('gulp'),
     sass = require('gulp-sass'),                       // 확실히 sass가 쓰기는 편해
 	sourcemaps = require('gulp-sourcemaps'),           // 소스맵 바인딩
 	autoprefixer = require('gulp-autoprefixer'),         // ie10은 지원해줘야...
-	notify = require('gulp-notify')                     // noti로 알려줍니다.
-
+	notify = require('gulp-notify');                     // noti로 알려줍니다.
 
 // path setting
 var path = {
     resource : './_resource/',
     its  : './source/its',
-    devServer : './.dev-server'
+    devServer : './.dev-server',
+    build : './build'
 };
 
 // 인메모리 스토리지로 잡파일 생성을 하지 않게 만들자
@@ -49,7 +51,7 @@ gulp.task('server:start', function() {
 });
 
 
-// 스타일 처리
+// ITS :: 개발 스타일 처리
 gulp.task('dev:its:style:sourcemap', function () {
 	return gulp.src(path.resource + '*.scss')
 		.pipe(sourcemaps.init({
@@ -71,17 +73,50 @@ gulp.task('dev:its:style:sourcemap', function () {
         .pipe(browserSync.stream());
 });
 
+// ITS :: build 스타일 처리
+gulp.task('build:its:style', function () {
+	return gulp.src(path.resource + '*.scss')
+		.pipe(sass({ errLogToConsole: false }))
+		.on('error', function(err) {
+			notify().write(err);
+			this.emit('end');
+		})
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'ie 10', 'ie 11'],
+			expand: true,
+			flatten: true
+		}))
+    //sass-sourcemap 작성. sourcemap에서 정확한 위치를 반영하려면 전처리가 모두 끝난 위치에 반영해주어야 한다.
+        .pipe(gulp.dest(path.build + '/resource'));
+});
 
-// 스크립트와 폰트파일 복사
+
+
+// ITS 개발 :: 스크립트와 폰트파일 복사
 gulp.task('dev:copy:asset',function () {
     return gulp.src(
         [
             path.resource + '/*',
             '!'+path.resource+'/*.scss'
         ]
-    ).pipe(gulp.dest(path.devServer + '/resource'));
+    )
+    .pipe(gulp.dest(path.devServer + '/resource'))
+    .pipe(browserSync.stream());
 });
 
+// ITS 빌드 :: 스크립트와 폰트파일 복사
+gulp.task('build:copy:asset',function () {
+    return gulp.src(
+        [
+            path.resource + '/*',
+            '!'+path.resource+'/*.scss'
+        ]
+    )
+    .pipe(gulp.dest(path.build + '/resource'));
+});
+
+
+// ITS 개발 :: html 파일
 gulp.task('dev:its:make-html',function () {
     var its_path = './source/its/'
     
@@ -90,19 +125,38 @@ gulp.task('dev:its:make-html',function () {
     .pipe(gulp.dest(path.devServer))
 });
 
-// 이미지 파일은 압축해서 넘김 
-gulp.task('dev:its:imagemin', function () {
+// ITS 빌드 :: html 파일
+gulp.task('build:its:make-html',function () {
+    var its_path = './source/its/'
+    
+    // html파일은 일단 복사만...
+    gulp.src(its_path + '/*.html') 
+    .pipe(stripComment())
+    .pipe(gulp.dest(path.build))
+});
+
+// ITS 개발 :: 이미지 파일 처리
+gulp.task('dev:its:image', function () {
     var its_path = './source/its/'
 
-    gulp.src(its_path + '**/*.png') 
-    .pipe(imagemin())
+    gulp.src(its_path + '**/*') 
     .pipe(gulp.dest(path.devServer))
+});
+
+// ITS 빌드 :: 이미지 파일은 압축해서 넘김 
+gulp.task('build:its:image', function () {
+    var its_path = './source/its/'
+
+    gulp.src(its_path + '**/*') 
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.build))
 });
 
 // watch option
 gulp.task('watch', function() {
     gulp.watch('./source/**/*.html',['dev:its:make-html']).on('change', browserSync.reload);
     gulp.watch(path.resource + "/*.scss", ['dev:its:style:sourcemap']);
+    gulp.watch(path.resource + "/*.js", ['dev:copy:asset']);
 });
 
 // 이제 뭘 만들건지 confirm을 받아야지? 매뉴얼 빌드가 워낙 여러개 있을거니까.
@@ -116,21 +170,22 @@ gulp.task('default', function(done) {
                 {
                     name : 'ITS',
                     value : 'its'
-                },
-                {
-                    name : 'MD',
-                    value : 'md'
-                },
-                {
-                    name : 'OpsNow',
-                    value : 'opsnow'
                 }
+                // ,
+                // {
+                //     name : 'MD',
+                //     value : 'md'
+                // },
+                // {
+                //     name : 'OpsNow',
+                //     value : 'opsnow'
+                // }
             ]
         },
         {
             type: 'list',
             name: 'devOrBuild',
-            message: '매뉴얼을 개발과 build중 고르세요',
+            message: '매뉴얼 개발하실건가요? build해서 배포하실건가요?',
             choices : [
                 {
                     name : '개발',
@@ -143,12 +198,17 @@ gulp.task('default', function(done) {
             ]
         }
     ]).then(function (answers) {
-        console.log(JSON.stringify(answers, null, '  '));
+        // console.log(JSON.stringify(answers, null, '  '));
 
         //답 받아왔지?
         if (answers.select === 'its' && answers.devOrBuild === 'dev') {
+            gulp.src(path.build, {read:false}).pipe(clean());
             gulp.start('dev:run:its');  //   ITS 개발모드 시작
-        
+        } else if (answers.select === 'its' && answers.devOrBuild === 'build') {
+            // 있던거 지워야지
+            gulp.src(path.its, {read:false}).pipe(clean());
+            console.log('build를 시작합니다.')
+            gulp.start('build:run:its')
         } else {
             console.log('현재는 지원하지 않습니다. 곧 만들게요 ㅜㅜ. 종료합니다!');
             done();
@@ -166,9 +226,15 @@ gulp.task('dev:run:its', [
     'server:start',
     'dev:its:style:sourcemap',
     'dev:copy:asset',
-    'dev:its:imagemin',
+    'dev:its:image',
     'dev:its:make-html'
+]);
 
+gulp.task('build:run:its', [
+    'build:its:style',
+    'build:copy:asset',
+    'build:its:image',
+    'build:its:make-html'
 ]);
 
 
@@ -181,8 +247,6 @@ gulp.task('dev:run:its', [
 //var	gulp = require('gulp');     //gulp 기초 불러오기
 //	// fs = require('fs'),
 //	// replace = require('gulp-replace'),
-//	// stripComment = require('gulp-strip-comments');
-
 
 
 
